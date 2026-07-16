4430e73ebfe34d972b959446c24f946e45d8aa20
Maison Matiere <maisonmatiere58@gmail.com>
2026-07-16 13:26:23 +0200

    Avis Google : lien direct de la fiche branche dans les demandes d'avis automatiques

--- js/email-config.js
+++ js/email-config.js
@@ -7,7 +7,7 @@ window.MM_EMAIL = {
   PUBLIC_KEY: '_eZElxudAzQEPqkXt',
   SERVICE_ID: 'service_s1f0gdj',
   TEMPLATE_ID: 'template_2ldl5el',
-  REVIEW_URL: '',                    // lien de votre fiche Google (avis), optionnel
+  REVIEW_URL: 'https://g.page/r/CTv9VnEVY0qbEBM/review',  // lien direct « laisser un avis » de la fiche Google
   ARTISAN_EMAIL: 'maisonmatiere58@gmail.com'  // reçoit les notifications internes (nouveau lead, etc.)
 };
 
